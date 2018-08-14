186e9329fce3da0c1d6cdd01e498451d97541763
Robert Foran <Robert.Foran@ucdconnect.ie>
2018-08-14 11:37:27 +0100

    DOM practice backup

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+const paraContent = document.createElement('p');
+paraContent.classList.add('content');
+paraContent.textContent = "Hey I'm red!";
+paraContent.style.color = 'red';
+container.appendChild(paraContent);
+const header3 = document.createElement('h3');
+header3.textContent = "I'm a blue h3"
+//header3.classList.add('blue'); This works don't know whhy
+header3.style.color='blue';
+const newdiv = document.createElement('div'); 
+newdiv.style.borderColor='black';
+newdiv.style.borderStyle='solid';
+newdiv.style.backgroundColor='pink';
+const header1 = document.createElement('h1');
+header1.textContent = "I'm in a div!"
+paraContent.textContent = "ME TOO!"
+paraContent.style.color = 'grey'
+newdiv.appendChild(header1);
+newdiv.appendChild(paraContent);
+container.appendChild(content);
+container.appendChild(header3);
+container.appendChild(newdiv);
+
+var btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World through the medium of javascript");
+/*
+var btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+  alert("Hello World through the medium of an Eventlistener!");
+});
+*/
+var btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click',  function(e){
+    console.log(e);
+    console.log(e.target);
+    e.target.style.background = 'blue'
+    alert("Hello World through the medium of an Eventlistener!");
+});
